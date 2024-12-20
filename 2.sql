40df436ccf3b650388a20a94b89471906c2150f6
Tanmay choudhari <89440891+tannmayyy@users.noreply.github.com>
2024-12-20 11:24:16 +0000

    changes 1

--- 2.sql
+++ 2.sql
@@ -1,29 +1,12 @@
--- Step 1: Get the column names from table3
-WITH Table3Columns AS (
-    SELECT COLUMN_NAME
-    FROM INFORMATION_SCHEMA.COLUMNS
-    WHERE TABLE_NAME = 'table3'
-),
--- Step 2: Split Uni_Combination values into individual components
-UniCombinations AS (
-    SELECT 
-        Insight_Numbers,
-        TRIM(value) AS Condition
-    FROM 
-        table1,
-        (SELECT SPLIT(Uni_Combination, '+') AS value FROM table1) AS split_values
-)
--- Step 3: Check if Uni_Combination values are present in table3 columns
 SELECT 
-    uc.Insight_Numbers,
-    uc.Condition AS Uni_Combination_Column,
-    CASE 
-        WHEN EXISTS (
-            SELECT 1 
-            FROM Table3Columns tc 
-            WHERE REPLACE(uc.Condition, ' ', '_') = tc.COLUMN_NAME
-        ) THEN 'Exists in table3'
-        ELSE 'Does not exist in table3'
-    END AS Existence_Status
+    t2.USI_ID,
+    t2.Insight_Numbers,
+    -- Split Uni_Combination by '+' into two columns
+    SPLIT(t1.Uni_Combination, '+')[0] AS Column1,
+    SPLIT(t1.Uni_Combination, '+')[1] AS Column2
 FROM 
-    UniCombinations uc;
+    table2 t2
+JOIN 
+    table1 t1 ON t2.Insight_Numbers = t1.Insight_Numbers
+WHERE 
+    t2.USI_ID = 'aditya';
