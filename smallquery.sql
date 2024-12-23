150f18185af740db9a9704634f5f11bf7ba4ed62
Tanmay choudhari <89440891+tannmayyy@users.noreply.github.com>
2024-12-23 11:59:43 +0530

    Update

---
+++ smallquery.sql
@@ -0,0 +1,15 @@
+SELECT 
+    t2.USI_ID,
+    t2.Insight_Numbers,
+    t3.COLUMN_NAME,
+    t3.COLUMN_VALUE
+FROM 
+    table2 t2
+JOIN 
+    table1 t1 
+    ON t2.Insight_Numbers = t1.Insight_Numbers
+JOIN 
+    table3 t3 
+    ON t3.COLUMN_NAME IN (SPLIT_PART(t1.Uni_Combination, '+', 1), SPLIT_PART(t1.Uni_Combination, '+', 2))
+WHERE 
+    t2.USI_ID = 'aditya';
